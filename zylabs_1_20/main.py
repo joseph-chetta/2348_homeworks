daa64d0df9809ba94da5e917a0c4f6654fd680df
joseph-chetta <70496130+joseph-chetta@users.noreply.github.com>
2020-09-16 01:52:22 -0500

    Add files via upload

---
+++ zylabs_1_20/main.py
@@ -0,0 +1,11 @@
+# Joseph Chetta 1640405
+
+print('Enter integer:')
+user_num1 = int(input())
+print('You entered:', user_num1)
+print('{} squared is {}'.format(user_num1, user_num1 * user_num1))
+print('And {} cubed is {} !!'.format(user_num1, user_num1**3))
+print('Enter another integer:')
+user_num2 = int(input())
+print('{} + {} is {}'.format(user_num1, user_num2, user_num1 + user_num2))
+print('{} * {} is {}'.format(user_num1, user_num2, user_num1 * user_num2))
