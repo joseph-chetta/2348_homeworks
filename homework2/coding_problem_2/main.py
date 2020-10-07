1b94135f9f1aa576ffd6ca1fc9d1fe2d1a512a0f
jchetta <j-chetta@hotmail.com>
2020-10-07 09:00:49 -0500

    Homework 2 c solution

--- homework2/coding_problem_2/main.py
+++ homework2/coding_problem_2/main.py
@@ -1,16 +1,21 @@
 # Joseph Chetta 1640405
 
-# Portion 'b' solution
+# Portion 'c' solution
+import datetime
+
 file = 'inputDates.txt'
 with open(file) as file_data:
     for line in file_data:
         if line == '-1':
             break
-        t = line.find('March 1, 1990')
-        if not t:
-            print('3/1/1990')
-        else:
-            # ignore
-            pass
-
+        # remove the newline at end
+        line = line.rstrip()
+        format = '%B %d, %Y'
+        try:
+            date = datetime.datetime.strptime(line, format)
+            new_date_format = "{}/{}/{}\n".format(date.month, date.day, date.year)
+            with open('parsedDates.txt', 'a+') as file:
+                file.write(new_date_format)
+        except ValueError:
+            print('Incorrect date format')
 
