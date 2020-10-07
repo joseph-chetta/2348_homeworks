8baa519453b01f8d193a5c8f0df455f2c897abd2
jchetta <j-chetta@hotmail.com>
2020-10-07 08:56:55 -0500

    Homework 2 problem portion a

---
+++ homework2/coding_problem_2/main.py
@@ -0,0 +1,14 @@
+# Joseph Chetta 1640405
+
+# Portion 'a' solution
+file = input()
+with open(file) as file_data:
+    for line in file_data:
+        if line == '-1':
+            break
+        t = line.find('March 1, 1990')
+        if not t:
+            print('3/1/1990')
+        else:
+            # ignore
+            pass
